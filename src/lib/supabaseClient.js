744575bc878266c384be2574611ca7723d3cbd2e
yuyuyu <feelvu@alt-a.net>
2024-11-01 21:50:06 +0900

    gitignore

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://klwenuauytemcqpcnivw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtsd2VudWF1eXRlbWNxcGNuaXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0NjExNjksImV4cCI6MjA0NjAzNzE2OX0.hmx8rAxDvkWo1jDDAKVoH6hHcsdDH1yWNeiYr8xzo6s';
-
+const SUPABASE_URL =
+    import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY =
+    import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
